c5c899bc88991d58d1851c9e5ad9cd7c670bb99d
ilaydayerlitas <79810814+ilaydayerlitas@users.noreply.github.com>
2022-03-17 13:45:12 +0300

    Update standardization.R

--- R/standardization.R
+++ R/standardization.R
@@ -227,35 +227,115 @@ std.tscore <- function(newdata, model,type = TRUE){
 
 
 
-std <- function(newdata, model, standardize = NULL, type = TRUE) {
+
+
+
+
+
+std.train <- function(data, standardize = NULL, type = TRUE) {
 
   if (any(standardize == "range")){
   
-   newdata <- std.range(newdata, model,type)
+    for (i in 1:ncol(data)){
+      
+      data[ , i] <- (data[ , i] - min(data[ , i])) /
+        (max(data[ , i]) - min(data[ , i]))
+      
+    }
   
   }
   else if (any(standardize == "zScore")){
   
-    newdata <- std.zscore(newdata, model,type)
+    for (i in 1:ncol(data)){
+      
+      data[ , i] <- (data[ , i] - mean(data[ , i])) / sd(data[ , i])
+      
+    }
   
   }
   else if (any(standardize == "tScore")){
     
-    newdata <- std.tscore(newdata, model,type)
+    for (i in 1:ncol(data)){
+      
+      data[ , i] <- (10 * ((data[ , i] - mean(data[ , i]))
+                              / sd(data[ , i]))) + 50
+      
+    }
   
   }
   else if (any(standardize == "mean")){
   
-    newdata <- std.mean(newdata, model,type)
+    for (i in 1:ncol(data)){
+      
+      data[ , i] <- data[ , i] / mean(data[ , i])
+      
+    }
   
   }
   else if (any(standardize == "deviance")){
   
-    newdata <- std.deviance(newdata, model,type)
+    for (i in 1:ncol(data)){
+      
+      data[ , i] <- data[ , i] / sd(data[ , i])
+      
+    }
   
+  }
+  
+  return(data)
+  }
+
+
+
+std.test<- function(newdata, model, standardize) {
+  
+  if (any(standardize == "range")){
+    
+    for (i in 1:ncol(newdata)){
+      
+      newdata[ , i] <- ((newdata[ , i] - model$fit$Std[i, 3]) /
+                          (model$fit$Std[i, 4] - model$fit$Std[i, 3]))
+      
+    }
+    
+  }
+  else if (any(standardize == "zScore")){
+    
+    for (i in 1:ncol(newdata)){
+      
+      newdata[ , i] <- (newdata[ , i] - model$fit$Std[i, 1]) /  model$fit$Std[i, 2]
+      
+    }
+    
+  }
+  else if (any(standardize == "tScore")){
+    
+    for (i in 1:ncol(newdata)){
+      
+      newdata[ , i] <- (10 * ((newdata[ , i] - model$fit$Std[i, 1])
+                              / model$fit$Std[i, 2])) + 50
+      
+    }
+    
+  }
+  else if (any(standardize == "mean")){
+    
+    for (i in 1:ncol(newdata)){
+      
+      newdata[ , i] <- newdata[ , i] / model$fit$Std[i, 1]
+      
+    }
+    
+  }
+  else if (any(standardize == "deviance")){
+    
+    for (i in 1:ncol(newdata)){
+      
+      newdata[, i] <- newdata[ , i] / model$fit$Std[i, 2]
+      
+    }
+    
   }
   
   return(newdata)
-  }
-
-
+}
