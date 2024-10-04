b7cc4c224b41acc63e9c2082746dee2d4823bb4d
gokmenzararsiz <gokmenzararsiz@erciyes.edu.tr>
2024-10-04 22:17:10 +0300

    auto create package

---
+++ R/createpackage.R
@@ -0,0 +1,7 @@
+library(devtools)
+pkgload::load_all()
+styler::style_pkg()
+usethis::use_mit_license()
+devtools::document()
+devtools::build_manual()
+devtools::build(args = "--compact-vignettes=gs+qpdf")
